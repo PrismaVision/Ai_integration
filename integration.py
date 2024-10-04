403c5f6b839594938b22648ff38d76aa3ee0898d
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2024-10-04 13:02:33 -0300

    Refactor prompt

--- integration.py
+++ integration.py
@@ -1,13 +1,38 @@
 import google.generativeai as genai
+import json
 
-genai.configure(api_key="{API_KEY}")
+genai.configure(api_key="")
 
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
+model = genai.GenerativeModel('gemini-1.5-flash')
 
-model = genai.GenerativeModel('gemini-pro')
+languages = ["en-us", "pt-br"]
 
-response = model.generate_content("Qual o codigo hex da cor azul")
+language = languages[0]
 
-print(response.text)
+colorTemperature = "cold, hot, neutral or earthy"
+
+#hex = "#FF0000"
+
+
+def search_color(hex):
+    jsonToFill = {
+    "Color": {
+        "id": None,
+        "name": None,
+        "hexCode": hex,
+        "rgbCode": None,
+        "rybPercents": None,
+        "colorimeter": None,
+        "colorDescription": None,
+        "twoColorsThatMatch": None,
+        "colorTerminology": None
+        }
+    }
+    json_string = json.dumps(jsonToFill)
+    
+    prompt = ("Complete this json:" + json_string + " \n considering the language: " + language + "\n, the hex code: " + hex + " \n and colorimeter: " + colorTemperature)
+    #print(prompt)
+    response = model.generate_content(prompt)
+    print(response.text)
+    
+search_color("#30D5C8")
