15f805a5adf74c9bedcf718a6702b37487975e2c
Bryan <bryanppfranca@gmail.com>
2024-10-09 20:37:36 -0300

    New RYB

--- integration.py
+++ integration.py
@@ -1,13 +1,13 @@
 import google.generativeai as genai
 import json
 
-genai.configure(api_key="")
+genai.configure(api_key="AIzaSyBAocn-QIdMLH5baVfbuWtvwvXIk_jG8rE")
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 languages = ["en-us", "pt-br"]
 
-language = languages[0]
+language = languages[1]
 
 colorTemperature = "cold, hot, neutral or earthy"
 
@@ -18,7 +18,7 @@ def search_color(hex) -> str:
         "name": None,
         "hexCode": hex,
         "rgbCode": None,
-        "rybPercents": None,
+        "RYBColorRecipe": None,
         "colorimeter": None,
         "colorDescription": None,
         "twoColorsThatMatch": None,
@@ -27,11 +27,11 @@ def search_color(hex) -> str:
     }
     json_string = json.dumps(jsonToFill)
     
-    prompt = ("Complete this json:" + json_string + " \n considering the language: " + language + "\n, the hex code: " + hex + " \n and colorimeter: " + colorTemperature)
-    #print(prompt)
+    prompt = (f"Complete this json: {json_string} \n considering the language: { language } \n, the hex code:  {hex}   \n and colorimeter:  {colorTemperature}")
+
     response = model.generate_content(prompt)
     responseFormated = response.text.replace("json", "").replace("```", "")
 
     print(responseFormated)
     
-search_color("#30D5C8")
+search_color("#d5e749")
